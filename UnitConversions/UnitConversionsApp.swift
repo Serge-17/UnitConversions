23ef93eb00dab9df3fcf46a202fab9a069630824
Serge-17 <sergei.eliseev17@icloud.com>
2024-09-11 11:49:14 +0300

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by Serge Eliseev on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnitConversionsTests/UnitConversionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsTests.swift
+//  UnitConversionsTests
+//
+//  Created by Serge Eliseev on 9/11/24.
+//
+
+import Testing
+@testable import UnitConversions
+
+struct UnitConversionsTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
